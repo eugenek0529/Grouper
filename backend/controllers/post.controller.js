abe316b082f2810e5748e572da84806879a48ee1
aziac <usjedi@gmail.com>
2024-11-07 17:33:33 -0800

    added basic filtering

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -46,10 +46,22 @@ export const createPost = async (req, res) => {
 }
 
 export const getPosts = async (req, res) => {
-  try{
-    const posts = await Post.find();
+  try {
+    const filter = {};
+
+    if (req.query.title) {
+      filter.title = { $regex: req.query.title, $options: "i" }; // 'i' for case-insensitive
+    }
+    if (req.query.creatorId) filter.creatorId = req.query.creatorId;
+    if (req.query.location) filter.location = req.query.location;
+    if (req.query.meetingStyle) filter.meetingStyle = req.query.meetingStyle;
+    if (req.query.tags) filter.tags = { $in: req.query.tags.split(",") };
+    if (req.query.capacity) filter.capacity = parseInt(req.query.capacity);
+    if (req.query.project_status) filter.project_status = req.query.project_status;
+
+    const posts = await Post.find(filter);
     res.status(200).json(posts);
-  } catch (err){
-    res.status(500).json({error: err.message});
+  } catch (err) {
+    res.status(500).json({ error: err.message });
   }
 }
